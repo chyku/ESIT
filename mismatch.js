56bbed1d9b8e9ff5298d57f3c2cc4ba7272c2407
chyku <ky.cu303@gmail.com>
2018-01-21 05:32:19 +0000

    tweaked comparison %s

--- mismatch.js
+++ mismatch.js
@@ -3,38 +3,48 @@ const fs = require('fs');
 
 var avg = 0.0;
 var occupancy = 0;
-var originpic = fs.readFileSync('./pictures/origin.jpg');
+var origin = resemble(fs.readFileSync('./pictures/origin.jpg'));
 var pic0 = fs.readFileSync('./pictures/0.jpg');
 var pic1 = fs.readFileSync('./pictures/1.jpg');
 var pic2 = fs.readFileSync('./pictures/2.jpg');
 var pictures = [pic0, pic1, pic2];
-var bounds = [{left: 334, top: 723, right: 799, bottom: 927}, {left: 531, top: 603, right: 860, bottom: 719}, {left: 731, top: 450, right: 1010, bottom: 550}]
-var areas = [921600 / (bounds[0].right - bounds[0].left)*(bounds[0].bottom - bounds[0].top), 0, 0, 0];
+var bounds = [{left: 60, top: 250, right: 450, bottom: 720}, {left: 650, top: 250, right: 1030, bottom: 720}, {left: 350, top: 0, right: 560, bottom: 250}, {left: 630, top: 0, right: 830, bottom: 250}];
+var areas = [5.03, 4.95, 17.55, 18.43];
 
-for (var i = 0; i < 3; i++){
-	origin = resemble(originpic);
-	
+for (var i = 0; i < 4; i++){
 	resemble.outputSettings({
 	  boundingBox: bounds[i],
 	  useCrossOrigin: false,
 	  outputDiff: true
 	});
 	
-	for (var j = 0; j < 3; j++) {
-		var diff = origin.compareTo(pictures[j]).onComplete(function(data){
-			console.log(data.misMatchPercentage);
-			avg += parseFloat(data.misMatchPercentage);
-		});
-	}
 	
-	avg = avg/3 * areas; // times proportion of area?
-	if (avg > 1){ //need better number here
-		occupancy += 25;
+	for (var j = 0; j < 3; j++) {
+		var mismatch = 0;
+		var diff = origin.compareTo(pictures[j]).onComplete(function(data){
+			mismatch = data.misMatchPercentage;
+		});
+		
+		console.log(mismatch);
+		avg += parseFloat(mismatch);
+	}
+		
+	avg = avg/3 * areas[i]; 
+	if (areas[i] > 10){
+		if (avg - 30 > 0){ //need better number here
+			occupancy += avg/35;
+		}
+	} else {
+		if (avg - 30 > 0){
+			occupancy += avg/40;
+		}
 	}
 	avg = 0;
 }
-
-console.log(occupancy);
+if (occupancy > 10} {
+	occupancy = 10;
+}
+console.log(occupancy*10);
 
 // if above certain percentage difference, add 25% to image or 1/5 to image
 /* var image = data.getImageDataUrl.toString();
