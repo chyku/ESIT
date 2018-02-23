b650ececbf2b85114a009e4e22a3dca4d990b95b
chyku <ky.cu303@gmail.com>
2018-02-23 10:10:24 -0500

    Create client.py

---
+++ PI2/client.py
@@ -0,0 +1,36 @@
+#CLIENT SIDE (PYTHON)
+
+import socket
+import time
+
+# Define host based on hostname -I
+# can't find IP address of other thing;
+# find before demo, git push and git pull real quick
+# run server on pi1
+
+HOST = "192.168.43.7"     # Symbolic name meaning all available interfaces
+PORT = 5007               # Arbitrary non-privileged port
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = (HOST, PORT)
+sock.connect(server_address)
+print ('Connected')
+try:
+    
+    # Send data
+    #!!! Define occupancy, change to string from int
+    msg = '54'
+    sock.send(msg.encode())
+    print ('Sending occupancy')
+
+    # Look for the response
+    answer = sock.recv(1024).decode()
+    #print(answer)       
+    if answer == 'received':
+    	print ("Received")
+
+finally:
+    sock.close()
