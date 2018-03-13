bbb09d2f79943bafa6909dd7b552d3cbbbd2e1b6
Kelley <k.chau175@gmail.com>
2018-03-13 14:22:42 +0000

    percent by input

--- PI2/graph.py
+++ PI2/graph.py
@@ -6,7 +6,8 @@ from matplotlib import animation
 fig=plt.figure()
 
 n=100 #Number of frames
-array = [50,20,10]
+percent = 0
+array = [50,20,percent]
 x = range(1,4)
 barcollection = plt.barh(x, array)
 
@@ -18,13 +19,15 @@ plt.title('Train Occupancy')
 plt.yticks([1, 2, 3])
 
 def animate(i):
-    y = [m + m for m in array]
-    for i in range(3):
-        array[i] += 1
+    # y = [m + m for m in array]
+    #for i in range(3):
+    #    array[i] += 1
+    percent = int(input('0 - 100: '))
+    y = [50, 20, percent]
     for i, b in enumerate(barcollection):
         b.set_width(y[i])
 
 # https://matplotlib.org/api/_as_gen/matplotlib.animation.FuncAnimation.html#matplotlib.animation.FuncAnimation
-anim=animation.FuncAnimation(fig,animate,repeat=False,blit=False,frames=n)
-
+# frames determines which percent it skips; why does it skip?
+anim=animation.FuncAnimation(fig,animate,repeat=True,blit=False,frames=500)
 plt.show()
--- PI2/server.py
+++ PI2/server.py
@@ -28,7 +28,7 @@ s.bind(server_address)
 s.listen(1)
 print('listening for a connection...')
 
-while(True):
+while(True): # basically should be all of the animate function
     try:
         #if a connection` is found, accept it and create (object, string)
         (CONNECTION, ADDRESS) = s.accept()
@@ -54,7 +54,6 @@ while(True):
             # graph code starts 
             fig=plt.figure()
 
-            n=100 #Number of frames
             occupancy = [50,20,10]
             x = range(1,4)
             barcollection = plt.barh(x, occupancy)
@@ -67,13 +66,11 @@ while(True):
             plt.yticks([1, 2, 3])
 
             def animate(i):
-                y = [m + m for m in occupancy]
-                for i in range(3):
-                    occupancy[i] += 1
+                percent = int(input('0 - 100: '))
+                y = [50, 20, percent]
                 for i, b in enumerate(barcollection):
                     b.set_width(y[i])
 
-            # https://matplotlib.org/api/_as_gen/matplotlib.animation.FuncAnimation.html#matplotlib.animation.FuncAnimation
             anim=animation.FuncAnimation(fig,animate,repeat=True,blit=False,frames=n)
 
             plt.show()
