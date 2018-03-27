b8c8cc3dd9cea4cab974a973ebc9697f439da5e3
chyku <ky.cu303@gmail.com>
2018-03-27 01:06:30 +0000

    adding no button bc i have no button rn

--- PI1/nobuttontest.py
+++ PI1/nobuttontest.py
@@ -1,16 +1,9 @@
-from gpiozero import Button
-import RPi.GPIO as IO
 import time
 import socket
 from signal import pause
 import os
 from Naked.toolshed.shell import execute_js, muterun_js
 
-IO.setwarnings(False)       #do not show any warnings
-IO.setmode (IO.BCM)         #we are programming the GPIO by BCM pin numbers. (PIN39 as 'GPIO19')
-IO.setup(19,IO.OUT)         # initialize GPIO19 as an output.
-IO.setup(26,IO.IN)               #initialize GPIO26 as input
-button = Button(26)
 
 # Define host based on hostname -I
 # can't find IP address of other thing;
@@ -23,8 +16,6 @@ PORT = 5007               # Arbitrary non-privileged port
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (HOST, PORT)
 
-camera = PiCamera()
-
 # might want to change origin address
 origin = '/home/pi/Desktop/project/pictures/origin.jpg'
 
@@ -33,25 +24,7 @@ def take_picture(pic):
     os.system("gpicview " + pic)
 
 while 1:
-    IO.output(19, True)
-    diff = 0
-    button.wait_for_press()
-    start_time = time.time()
-    
-    while button.is_active:
-        now_time = time.time()
-        diff = -start_time+now_time
-
-    if diff >= 5 :           #long hold
-        camera.capture(origin)
-
-        # where to light up LED
-        IO.output(19, True)
-        time.sleep(1)
-        IO.output(19,False)
-        
-    else:                   #short hold
-        time.sleep(5)
+        send = input("press enter to send")
 
         for i in range(3):
             take_picture('/home/pi/Desktop/project/pictures/'+ str(i) + '.jpg')
