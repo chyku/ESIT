70677a1b0c2b6193c62cf7aebb31681fd19215ed
Kelley <k.chau175@gmail.com>
2018-03-27 14:15:37 +0000

    merging edits, changing address

--- PI1/client.py
+++ PI1/client.py
@@ -1,39 +1,72 @@
-#CLIENT SIDE (PYTHON)
-
-import socket
+from gpiozero import Button
+from picamera import PiCamera
+import RPi.GPIO as IO
 import time
+import socket
+from signal import pause
+import os
+from Naked.toolshed.shell import execute_js, muterun_js
 
-# Define host based on hostname -I
+IO.setwarnings(False)       # do not show any warnings
+IO.setmode (IO.BCM)         # we are programming the GPIO by BCM pin numbers. (PIN39 as 'GPIO19')
+IO.setup(19,IO.OUT)         # initialize GPIO19 as an output.
+IO.setup(26,IO.IN)          # initialize GPIO26 as input
+button = Button(26)
+
+# Define host based on hostname -I (PI2 IP address)
 # can't find IP address of other thing;
 # find before demo, git push and git pull real quick
-# run server on pi1
+# run server on pi2
 
-HOST = "192.168.43.158"     # Symbolic name meaning all available interfaces
+HOST = "192.168.43.7"     # Symbolic name meaning all available interfaces
 PORT = 5007               # Arbitrary non-privileged port
 
+camera = PiCamera()
 
+# might want to change origin address
+origin = '/home/pi/Desktop/pictures/origin.jpg'
 
-while (1):
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def take_picture(pic):
+    time.sleep(2)
+    camera.capture(pic)
 
-    # Connect the socket to the port where the server is listening
-    server_address = (HOST, PORT)
-    sock.connect(server_address)
-    print ('Connected')
-    try:
+while 1:
+    diff = 0
+    button.wait_for_press()
+    start_time = time.time()
+    
+    while button.is_active:
+        now_time = time.time()
+        diff = -start_time+now_time
+
+    if diff >= 5 :           #long hold
+        IO.output(19, True)
+        time.sleep(.5)
+        camera.capture(origin)
+        IO.output(19, False)
         
-        # Send data
-        #!!! Define occupancy, change to string from int
-        msg = str(input())
-        sock.send(msg.encode())
-        print ('Sending occupancy')
+    else:                   #short hold
+        IO.output(19, True)
+        time.sleep(5)
 
-        """ Look for the response (confirmation)
-        answer = sock.recv(1024).decode()
-        #print(answer)       
-        if answer == 'received':
-            print ("Received") """
+        for i in range(3):
+            take_picture('/home/pi/Desktop/pictures/'+ str(i) + '.jpg')
 
-    finally:
-        sock.close()
+        response = muterun_js('mismatch.js')
+
+        if response.exitcode == 0:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = (HOST, PORT)
+            sock.connect(server_address)
+            
+            try:
+                data = str(int(float((response.stdout)[0:-1])))
+                sock.send(data.encode())
+                """ Look for the response (confirmation)
+                answer = sock.recv(1024).decode()
+                #print(answer)       
+                if answer == 'received':
+                print ("Received") """
+            finally:
+                sock.close()
+        IO.output(19, False)
