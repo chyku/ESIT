ff66a1fa1ed492865d0bf4bd85e492d7a2553f8c
Kelley <k.chau175@gmail.com>
2018-05-11 14:27:09 +0000

    jk

--- PI2/sheets.py
+++ PI2/sheets.py
@@ -1,30 +1,58 @@
 """
-Shows basic usage of the Sheets API. Prints values from a Google Spreadsheet.
+BEFORE RUNNING:
+---------------
+1. If not already done, enable the Google Sheets API
+   and check the quota for your project at
+   https://console.developers.google.com/apis/api/sheets
+2. Install the Python client library for Google APIs by running
+   `pip install --upgrade google-api-python-client`
 """
 from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-# Setup the Sheets API
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-store = file.Storage('credentials.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-service = build('sheets', 'v4', http=creds.authorize(Http()))
+from pprint import pprint
 
-# Call the Sheets API
-SPREADSHEET_ID = '1Cyxfg7UIq2_1-mnb55ibDHYYxznCUos1RNqyq_ff2Y0'
-RANGE_NAME = 'Class Data!A2:E'
-result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
-                                             range=RANGE_NAME).execute()
-values = result.get('values', [])
-if not values:
-    print('No data found.')
-else:
-    print('Name, Major:')
-    for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s' % (row[0], row[4]))
+from googleapiclient import discovery
+
+# TODO: Change placeholder below to generate authentication credentials. See
+# https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
+#
+# Authorize using one of the following scopes:
+#     'https://www.googleapis.com/auth/drive'
+#     'https://www.googleapis.com/auth/drive.file'
+#     'https://www.googleapis.com/auth/spreadsheets'
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+store = file.Storage('credentials.json')
+credentials= store.get()
+if not credentials or credentials.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    credentials = tools.run_flow(flow, store)
+    
+service = build('sheets', 'v4', http=credentials.authorize(Http()))
+
+service = discovery.build('sheets', 'v4', credentials=credentials)
+
+# The ID of the spreadsheet to update.
+spreadsheet_id = '1Cyxfg7UIq2_1-mnb55ibDHYYxznCUos1RNqyq_ff2Y0'
+# The A1 notation of the values to update.
+range_ = 'Sheet1!A1:D4'  # TODO: Update placeholder value.
+
+# How the input data should be interpreted.
+value_input_option = 'USER_ENTERED'  # TODO: Update placeholder value.
+
+value_range_body = {
+    # TODO: Add desired entries to the request body. All existing entries
+    # will be replaced.
+    {"Item", "Cost", "Stocked", "Ship Date"},
+    {"Wheel", "$20.50", "4", "3/1/2016"},
+    {"Door", "$15", "2", "3/15/2016"},
+    {"Engine", "$100", "1", "30/20/2016"}
+}
+
+request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
+response = request.execute()
+
+# TODO: Change code below to process the `response` dict:
+pprint(response)
