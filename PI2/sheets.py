a93bdddddf6f8f0ff7c8324b31676a9e710d1374
Kelley <k.chau175@gmail.com>
2018-05-22 14:25:48 +0000

    now appending; need to make arrays for timestamps

--- PI2/sheets.py
+++ PI2/sheets.py
@@ -13,18 +13,14 @@ from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import time
+import datetime
 
 from pprint import pprint
 
 from googleapiclient import discovery
 
-# TODO: Change placeholder below to generate authentication credentials. See
-# https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
-#
-# Authorize using one of the following scopes:
-#     'https://www.googleapis.com/auth/drive'
-#     'https://www.googleapis.com/auth/drive.file'
-#     'https://www.googleapis.com/auth/spreadsheets'
+# Authentication credentials
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 store = file.Storage('credentials.json')
 credentials= store.get()
@@ -37,12 +33,18 @@ service = build('sheets', 'v4', http=credentials.authorize(Http()))
 service = discovery.build('sheets', 'v4', credentials=credentials)
 
 # The ID of the spreadsheet to update.
-spreadsheet_id = '1Cyxfg7UIq2_1-mnb55ibDHYYxznCUos1RNqyq_ff2Y0'
+spreadsheet_id = '1aiT2VB92fMMpK9RFkC6y7WStEurKKNWohLWG0sP2tgo'
 # The A1 notation of the values to update.
-range_ = 'Sheet1!A1:B2'  # TODO: Update placeholder value.
+range_ = 'Sheet1!A1:B5'  # TODO: Update placeholder value.
 
 # How the input data should be interpreted.
-value_input_option = 'USER_ENTERED'  # TODO: Update placeholder value.
+value_input_option = 'USER_ENTERED'
+# Append vs replace
+insert_data_option = 'INSERT_ROWS'
+
+# setup done
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 value_range_body = {
     "range": range_, # The range the values cover, in A1 notation.
@@ -55,12 +57,13 @@ value_range_body = {
       # will set `A1=1,B1=2,A2=3,B2=4`. With `range=A1:B2,majorDimension=COLUMNS`
       # then `[[1,2],[3,4]]` will set `A1=1,B1=3,A2=2,B2=4`.
       # To set a cell to an empty value, set the string value to an empty string.
-      [1,2],[3,4]
+      [st,st],[3,4]
     ],
     "majorDimension": "COLUMNS"
 }
 
-request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
+# request = service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, body=value_range_body)
+request = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, insertDataOption=insert_data_option, body=value_range_body)
 response = request.execute()
 
 # TODO: Change code below to process the `response` dict:
